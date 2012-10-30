eea57f5d0fea110b4ba4a227b35b3203cf543bca
vivia <jhezjkp@gmail.com>
2012-10-30 15:15:44 +0800

    增加随机网址缩写测试

--- tests/test_url.py
+++ tests/test_url.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 #encoding=utf-8
 
+import os
+import random
 import unittest
 from shorturl import url
 
@@ -9,5 +11,9 @@ class UrlTestCase(unittest.TestCase):
 
     def testShortenURL(self):
         self.assertEqual(url.shortenURL("www.163.com"), ['mtKj7', 'v1T3x', 'rSWsK', 'pGl2-'])
+
+    def testShortenURLRandom(self):
+        for i in range(1, 100):
+            self.assertEqual(len(url.shortenURL(os.urandom(random.randint(5, 32)).encode('hex'))), 4)
 
 
